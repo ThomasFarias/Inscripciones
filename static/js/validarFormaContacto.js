e1d4e75da2ee879fd45f8e4e1f717b5132cbead6
Tatarkus <matias.trks@gmail.com>
2019-03-12 18:58:59 -0300

    intento validator

--- static/js/validarFormaContacto.js
+++ static/js/validarFormaContacto.js
@@ -1,12 +1,39 @@
-function validarFormaContacto() {
-  var nombre = document.forms["formaContacto"]["nombre"]; 
-  var telefono =document.forms["formaContacto"]["telefono"] ;
-  var mensaje = document.forms["formaContacto"]["mensaje"];
-  var correo = document.forms["formaContacto"]["correo"];
-  var motivo = document.forms["formaContacto"]["motivo"].index;
+$.validator.setDefaults( {
+      submitHandler: function () {
+        alert( "submitted!" );
+      }
+    } );
 
-  if (nombre == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
+    $( document ).ready( function () {
+      $( "#formaContacto" ).validate( {
+        rules: {
+          nombre: "required",
+          correo: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          nombre: "Inserta el nombre, weon",
+          email: "Please enter a valid email address"
+        },
+        errorElement: "em",
+        errorPlacement: function ( error, element ) {
+          // Add the `invalid-feedback` class to the error element
+          error.addClass( "invalid-feedback" );
+
+          if ( element.prop( "type" ) === "checkbox" ) {
+            error.insertAfter( element.next( "label" ) );
+          } else {
+            error.insertAfter( element );
+          }
+        },
+        highlight: function ( element, errorClass, validClass ) {
+          $( element ).addClass( "is-invalid" ).removeClass( "is-valid" );
+        },
+        unhighlight: function (element, errorClass, validClass) {
+          $( element ).addClass( "is-valid" ).removeClass( "is-invalid" );
+        }
+      } );
+
+    } );
