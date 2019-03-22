97f64fa87d8a8aa5548527c8099bd64ca9d5dd9e
Tomás <tomas.farias.do@outlook.com>
2019-03-21 22:58:44 -0300

    Sitio web con Java

---
+++ web/static/js/validarFormaContacto.js
@@ -0,0 +1,43 @@
+$.validator.setDefaults( {
+      submitHandler: function () {
+        alert( "submitted!" );
+      }
+    } );
+
+    $( document ).ready( function () {
+      $( "#formaContacto" ).validate( {
+        rules: {
+          nombre: "required",
+          correo: {
+            required: true,
+            email: true
+          },
+          motivo:"required"
+          
+        },
+        messages: {
+          nombre: "Debe ingresar un nombre.",
+          correo: "Debe ingresar un correo valido.",
+          motivo:"Debe seleccionar una motivo."
+          
+        },
+        errorElement: "em",
+        errorPlacement: function ( error, element ) {
+          // Add the `invalid-feedback` class to the error element
+          error.addClass( "invalid-feedback" );
+
+          if ( element.prop( "type" ) === "checkbox" ) {
+            error.insertAfter( element.next( "label" ) );
+          } else {
+            error.insertAfter( element );
+          }
+        },
+        highlight: function ( element, errorClass, validClass ) {
+          $( element ).addClass( "is-invalid" ).removeClass( "is-valid" );
+        },
+        unhighlight: function (element, errorClass, validClass) {
+          $( element ).addClass( "is-valid" ).removeClass( "is-invalid" );
+        }
+      } );
+
+    } );
