48bffd4def2386eff99b28f20e6081fd8f922eba
Tatarkus <matias.trks@gmail.com>
2019-03-26 18:11:15 -0300

    servlet procesa nombre

--- src/java/requests/ServletForm.java
+++ src/java/requests/ServletForm.java
@@ -7,6 +7,7 @@ package requests;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,18 +31,20 @@ public class ServletForm extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletForm</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletForm at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        String nombre;
+        String email;
+        String telefono;
+        String mensaje;
+        Boolean valido = true;
+        
+        nombre = request.getAttribute("nombre").toString(); 
+        if(nombre == "")
+            valido = false;
+        if(valido)
+        {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/requests/respuestaform.jsp");
+                rd.forward(request, response);
+        }        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
