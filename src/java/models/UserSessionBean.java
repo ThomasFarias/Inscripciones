657959c0018f4a3daab36e45691b231a4f33d878
ThomasFarias <42778501+ThomasFarias@users.noreply.github.com>
2019-04-02 18:55:20 -0300

    Contacto y User SessionBean

---
+++ src/java/models/UserSessionBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author cetecom
+ */
+@Stateless
+public class UserSessionBean implements UserSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
